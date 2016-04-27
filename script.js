bc682aae57b5cba2cf23323c82b7eb7074e2057b
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-27 11:27:13 -0400

    Added multiple views

--- script.js
+++ script.js
@@ -5,7 +5,9 @@
   .module("app", [
     "ui.router"
   ])
-  .config(Config);
+  .config(Config)
+  .controller("ctrlCollection", CtrlCollection)
+  .controller("ctrlStatic", CtrlStatic);
   
   Config.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
   function Config($stateProvider, $locationProvider, $urlRouterProvider){
@@ -13,18 +15,30 @@
       $locationProvider.html5Mode(true);
     }
     $stateProvider
-    .state("main", {
-      url: "/"
+    .state("collection", {
+      url: "/collection/:a",
+      templateUrl: "/html/collection.html",
+      controller: "ctrlCollection"
     })
-    .state("cxn", {
-      url: "/collection/:a"
-    })
-    .state("abt", {
-      url: "/about/:a"
-    })
-    .state("contact", {
-      url: "/contact"
+    .state("static", {
+      url: "/:a",
+      template: "<div data-ng-include='templateUrl()'></div>",
+      controller: "ctrlStatic"
     });
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/home");
+  }
+  
+  CtrlCollection.$inject = ["$scope", "$stateParams"];
+  function CtrlCollection($scope, $stateParams){
+    var vm = $scope;
+    vm.title  = $stateParams.a;
+  }
+  
+  CtrlStatic.$inject = ["$scope", "$stateParams"];
+  function CtrlStatic($scope, $stateParams){
+    var vm = $scope;
+    vm.templateUrl = function(){
+      return("/html/" + $stateParams.a + ".html");
+    }
   }
 })();
