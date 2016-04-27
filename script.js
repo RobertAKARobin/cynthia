8825846747933d1fb5da603ad5a5744806fa335e
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-27 05:06:10 -0400

    Added Angular routing

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 "use strict";
 
-$(document).ready(function(){
-  console.log("Hello, world!");
-});
+(function(){
+  angular
+  .module("app", [
+    "ui.router"
+  ])
+  .config(Config);
+  
+  Config.$inject = ["$stateProvider", "$urlRouterProvider"];
+  function Config($stateProvider, $urlRouterProvider){
+    $stateProvider
+    .state("main", {
+      url: "/"
+    })
+    .state("cxn", {
+      url: "collection/:a"
+    })
+    .state("abt", {
+      url: "about/:a"
+    })
+    .state("contact", {
+      url: "contact"
+    });
+    $urlRouterProvider.otherwise("/");
+  }
+})();
